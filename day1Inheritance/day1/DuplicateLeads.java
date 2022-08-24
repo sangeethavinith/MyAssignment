adb0f31df1bbde62df6fa7c73711c2120942fe2e
sangeethavinith <110752832+sangeethavinith@users.noreply.github.com>
2022-08-24 17:45:09 +0530

    Add files via upload
    week2.day1

---
+++ day1Inheritance/day1/DuplicateLeads.java
@@ -0,0 +1,37 @@
+package Week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DuplicateLeads {
+public static void main(String[] args) {
+	WebDriverManager.chromedriver().setup();
+	ChromeDriver driver=new ChromeDriver();
+	driver.get("http://leaftaps.com/opentaps/control/login");
+	driver.manage().window().maximize();
+    //.manage().timeouts().
+    driver.findElement(By.id("username")).sendKeys("demosalesManager");
+    driver.findElement(By.id("password")).sendKeys("crmsfa");
+    driver.findElement(By.className("decorativeSubmit")).click();
+    driver.findElement(By.linkText("CRM/SFA")).click();
+    driver.findElement(By.linkText("Leads")).click();
+    driver.findElement(By.linkText("Create Lead")).click();
+    driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Test Leaf");
+    driver.findElement(By.id("createLeadForm_firstName")).sendKeys("sivasangeeth");
+    driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Marimuthu");
+    driver.findElement(By.id("createLeadForm_departmentName")).sendKeys("IT");
+	driver.findElement(By.id("createLeadForm_description")).sendKeys("Testing");
+	driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("1234qwer@gmail.com");
+	driver.findElement(By.className("smallSubmit")).click();
+	driver.findElement(By.linkText("DuplicteLead")).click();
+	driver.findElement(By.id("createLeadForm_companyName")).clear();
+    driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Test Leaf2");
+    driver.findElement(By.id("createLeadForm_lastName")).clear();
+    driver.findElement(By.id("createLeadForm_lastmentName")).sendKeys("arun");
+    driver.findElement(By.className("smallSubmit")).click();
+	String title=driver.getTitle();
+	System.out.println(title);
+}
+}
